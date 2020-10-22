574b2f5427509105ee6d627a9a0fd872d79d233d
alfredopl <aalfredoplz@gmail.com>
2020-10-22 11:13:39 -0300

    Completed method numero_cero

--- patrones.rb
+++ patrones.rb
@@ -29,6 +29,22 @@ def letra_x(n)
         end
     end
 end
+def numero_cero(n)
+    n.times do |i|
+        if i%5==4 || i%5==0
+            puts "*****"
+        elsif
+            i%5==1
+            puts "**  *"
+        elsif
+            i%5==3
+            puts "*  **"
+        elsif
+            i%5==2
+            puts "* * *"
+        end
+    end
+end
 n = ARGV[0].to_i
 
-letra_x(n)
+numero_cero(n)
