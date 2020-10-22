2e445477974b4b644fbcc0cae2d8dcaabe36dbdb
alfredopl <aalfredoplz@gmail.com>
2020-10-22 11:25:39 -0300

    Completed method navidad

--- patrones.rb
+++ patrones.rb
@@ -45,6 +45,24 @@ def numero_cero(n)
         end
     end
 end
+def navidad(n)
+    n.times do |i|
+        if i%5==1
+            puts "* * * *"
+        elsif
+            i%5==2||i%5==3 
+            puts "   *"
+        elsif
+            i%5==0
+            puts "   *"
+            puts "  * *"
+            puts " * * *"
+        elsif
+            i%5==4
+            puts " * * *"
+        end
+    end
+end
 n = ARGV[0].to_i
 
-numero_cero(n)
+navidad(n)
