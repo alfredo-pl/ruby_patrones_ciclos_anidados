0274eb1be1f0a0b1f41f14232e0c6507f04e056a
alfredopl <aalfredoplz@gmail.com>
2020-10-22 11:51:58 -0300

    Completed patrones.rb and menu

--- patrones.rb
+++ patrones.rb
@@ -63,6 +63,35 @@ def navidad(n)
         end
     end
 end
-n = ARGV[0].to_i
+puts"Bienvenido escoja el metodo que desea usar"
+puts"\nMenu:
+1: LETRA O
+2: LETRA I
+3: LETRA Z
+4: LETRA X
+5: NUMERO 0
+6: NAVIDAD
+"
+metodo=gets.chomp.to_i
 
-navidad(n)
+if metodo > 7 || metodo== 0
+    puts"Opción invalida vuelva a iniciar"
+elsif metodo.class == 'Integer'
+    puts"No es un número vuelva a iniciar"
+else
+    puts"ahora ingrese la cantidad de lineas"
+n=gets.chomp.to_i
+    if metodo==1
+        letra_o(n)
+    elsif metodo==2
+        letra_i(n)
+    elsif metodo==3
+        letra_z(n)
+    elsif metodo==4
+        letra_x(n)
+    elsif metodo==5
+        numero_cero(n)
+    else
+        navidad(n)
+    end
+end
