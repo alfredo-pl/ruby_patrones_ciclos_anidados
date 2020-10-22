91c1d40caa34a50ae89bc83daf2e64b4eea49dd3
alfredopl <aalfredoplz@gmail.com>
2020-10-22 11:03:15 -0300

    Completed method letra_i

--- patrones.rb
+++ patrones.rb
@@ -7,7 +7,16 @@ def letra_o(n)
         end
     end
 end
+def letra_i(n)
+    n.times do |i|
+        if i%5==4 || i%5==0
+            puts "*****"
+        else
+            puts "  *"
+        end
+    end
+end
 
 n = ARGV[0].to_i
 
-letra_o(n)
+letra_i(n)
